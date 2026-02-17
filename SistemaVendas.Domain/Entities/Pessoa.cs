f0f893062bcffe26f578d8491cd05590ec03aa64
IgorDias1998 <igordiass1998@gmail.com>
2026-02-17 07:56:08 -0300

    Adicionando as entidades cliente e endereço

---
+++ SistemaVendas.Domain/Entities/Pessoa.cs
@@ -0,0 +1,13 @@
+﻿namespace SistemaVendas.Domain.Entities
+{
+    public class Pessoa
+    {
+        public Guid ClienteId { get; set; } = Guid.NewGuid();
+        public string NomeCliente { get; set; } = string.Empty;
+        public string EmailCliente { get; set; } = string.Empty;
+        public DateTime DataNascimento { get; set; }
+        public string TelefoneCliente { get; set; } = string.Empty;
+        public string DocumentoCliente { get; set; } = string.Empty;
+        public Endereco EnderecoCliente { get; private set; } = null!;
+    }
+}
