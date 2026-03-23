b77c40508d09e9faac16ee3017de8e928902c76d
IgorDias1998 <igordiass1998@gmail.com>
2026-03-23 08:23:55 -0300

    feature: início do repositório

---
+++ SistemaVendas.Application/Interfaces/IVendaRepository.cs
@@ -0,0 +1,14 @@
+﻿using SistemaVendas.Domain.Entities;
+
+namespace SistemaVendas.Application.Interfaces
+{
+    public interface IVendaRepository
+    {
+        public Task AdicionarVendaAsync(Venda venda);
+        public Task<IEnumerable<Venda>> BuscarVendasAsync();
+        public Task<Venda?> BuscarVendaPorIdAsync(int vendaId);
+        public Task AtualizarVendaAsync(Venda venda);
+        public Task DeletarVendaAsync(int vendaId);
+        Task AdicionarListaVendaAsync(List<Venda> vendas);
+    }
+}
---
+++ SistemaVendas.Infrastructure/Repositories/VendaRepository.cs
@@ -0,0 +1,46 @@
+﻿using SistemaVendas.Application.Interfaces;
+using SistemaVendas.Domain.Entities;
+using SistemaVendas.Infrastructure.Persistence;
+
+namespace SistemaVendas.Infrastructure.Repositories
+{
+    public class VendaRepository : IVendaRepository
+    {
+        private readonly AppDbContext _context;
+
+        public VendaRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task AdicionarListaVendaAsync(List<Venda> vendas)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AdicionarVendaAsync(Venda venda)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AtualizarVendaAsync(Venda venda)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Venda?> BuscarVendaPorIdAsync(int vendaId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Venda>> BuscarVendasAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeletarVendaAsync(int vendaId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
